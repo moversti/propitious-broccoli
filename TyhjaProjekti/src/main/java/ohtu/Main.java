e058831aacf9b90a36afaec57666a79c5b3d741f
Mikko Översti <mikko.oversti@helsinki.fi>
2015-09-30 14:13:23 +0300

    teht2 aloitettu annetulla pohjalla ja portattu käyttämään org.apache.httpcomponents httpclient commonssin sijaan

--- TyhjaProjekti/src/main/java/ohtu/Main.java
+++ TyhjaProjekti/src/main/java/ohtu/Main.java
@@ -1,7 +1,37 @@
 package ohtu;
+import com.google.gson.Gson;
+import java.io.IOException;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.util.EntityUtils;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        String studentNr = "012345678";
+        if ( args.length>0) {
+            studentNr = args[0];
+        }
+
+        String url = "http://ohtustats-s2015.herokuapp.com/students/"+studentNr+"/submissions";
+
+        CloseableHttpClient client = HttpClients.createDefault();
+        HttpGet get = new HttpGet(url);
+        CloseableHttpResponse execute = client.execute(get);
+        String bodyText = EntityUtils.toString(execute.getEntity());
+
+        System.out.println("json-muotoinen data:");
+        System.out.println( bodyText );
+
+        Gson mapper = new Gson();
+        Submission[] subs = mapper.fromJson(bodyText, Submission[].class);
+
+        System.out.println("Oliot:");
+        for (Submission submission : subs) {
+            System.out.println(submission);
+        }
+
     }
 }
